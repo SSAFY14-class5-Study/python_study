0e92f6551ca440b49b179c0891345df776c09ec8
hge2ne <hge2ne@gmail.com>
2025-08-23 13:11:08 +0900

    [혜진] Sol: SW1945.py

---
+++ week3/SW1945.py
@@ -0,0 +1,32 @@
+# 간단한 소인수 분해
+
+T = int(input())
+
+for tc in range(1, T + 1):
+    N = int(input())
+    a = 0
+    b = 0
+    c = 0
+    d = 0
+    e = 0
+
+    while N % 2 == 0:
+        a += 1
+        N = N // 2
+    while N % 3 == 0:
+        b += 1
+        N = N // 3
+
+    while N % 5 == 0:
+        c += 1
+        N = N // 5
+
+    while N % 7 == 0:
+        d += 1
+        N = N // 7
+
+    while N % 11 == 0:
+        e += 1
+        N = N // 11
+
+    print(f"#{tc} {a} {b} {c} {d} {e}")
