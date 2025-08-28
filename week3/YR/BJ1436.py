04bf4bc606477aec75b918816a56d8f44c891b07
Yerimmm-Cho <rim000514@gmail.com>
2025-08-28 17:29:09 +0900

    [예림] Sol: BJ1436.py

---
+++ week3/YR/BJ1436.py
@@ -0,0 +1,13 @@
+# 영화감독 숌
+N = int(input())  
+
+cnt = 0
+num = 666
+
+while True:
+    if '666' in str(num):
+        cnt += 1
+        if cnt == N:
+            print(num)
+            break
+    num += 1
