75462b64909a33f71d6b4d871adec871b6a3002d
yerimcho <rim000514@gmail.com>
2025-09-03 17:35:29 +0900

    [예림] Sol: SW6485.py

--- week4/YR/SW6485.py
+++ week4/YR/SW6485.py
@@ -16,12 +16,15 @@ for tc in range(1, T+1):
         num = int(input()) 
         C.append(num)
 
-    arr = [0] + [0] * P
+    arr = [0] * 5001
 
     for i in range(N):
         for j in range(A[i], B[i] + 1):
             arr[j] += 1
 
 
-    result = arr[1:]
+    result = []
+    for k in C:
+        result.append(arr[k])
+
     print(f"#{tc}", *result)
